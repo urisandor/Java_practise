a76d314fa632b90ceb7e46f91c7fadf7722bc56d
urisandor <urisandor14@gmail.com>
2024-10-09 17:55:23 +0200

    finish
    NIce

--- exercises/egymasutani.java
+++ exercises/egymasutani.java
@@ -2,6 +2,37 @@ package exercises;
 
 public class egymasutani {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        int[] values = { 1, 4, 7, 7, 7, 7 };
+        int[] values1 = { 7, 7, 7, 7 };
+        int[] values2 = {};
+        int[] values3 = { 1, 4, 7 };
+        int[] values4 = { 1, 4, 7, 7 };
+        System.out.println(isConsecutiveFour(values));
+        System.out.println(isConsecutiveFour(values1));
+        System.out.println(isConsecutiveFour(values2));
+        System.out.println(isConsecutiveFour(values3));
+        System.out.println(isConsecutiveFour(values4));
+
+    }
+
+    public static boolean isConsecutiveFour(int[] values) {
+        if (values.length < 4) {
+            return false;
+        }
+        int previous = values[0];
+        int count = 1;
+        for (int i = 1; i < values.length; i++) {
+            if (values[i] == previous) {
+                count++;
+            } else {
+                previous = values[i];
+            }
+        }
+
+        if (count >= 4) {
+            return true;
+        }
+        return false;
     }
 }
