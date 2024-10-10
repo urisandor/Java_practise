2a28663ce4b5fbd2fdcd497112084ddb65a22485
urisandor <urisandor14@gmail.com>
2024-10-10 17:14:16 +0200

    Update lotto.java

--- exercises/lotto.java
+++ exercises/lotto.java
@@ -1,9 +1,11 @@
 package exercises;
 
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
-
+import java.util.Collections;
+import java.util.List;
 
 
 public class lotto {
@@ -26,24 +28,38 @@ public class lotto {
     for (int num : nums) {
         System.out.println(num);
     }
-    
-    
-    /*for (int num : nums) {
-        System.out.println(num);
-    }*/
     input.close();
+
+
     }
 
     public static int[] Myrandom(int hi, int lo, int n){
-        int nums[] = new int[n]; //// Tömb inicializálása a megfelelő mérettel
+        /*int nums[] = new int[n]; //// Tömb inicializálása a megfelelő mérettel
         Random rand = new Random();
+        int current;
         //int num = rand.nextInt((hi - lo) + lo);
         for (int index = 0; index < n; index++) {
-            nums[index] = rand.nextInt((hi - lo +1)) + lo;
-            
+            current = rand.nextInt((hi - lo +1)) + lo;
+            nums[index] = current;      
         }
+        return nums;*/
         
-        return nums;
-        
+
+        // Létrehozzuk az összes lehetséges számot tartalmazó listát
+        List<Integer> allNumbers = new ArrayList<>();
+        for (int i = lo; i <= hi; i++) {
+            allNumbers.add(i);
+        }
+
+        // Véletlenszerűen összekeverjük a listát
+        Collections.shuffle(allNumbers, new Random());
+
+        // Az első n számot kiválasztjuk a listából
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            result[i] = allNumbers.get(i);
+        }
+
+        return result;
     }
 }
